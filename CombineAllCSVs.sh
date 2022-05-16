407805651f6072bcc303ea269415f22608188141
drcarademono <73616371+drcarademono@users.noreply.github.com>
2022-05-15 17:32:03 -0700

    Update CombineAllCSVs.sh

--- CombineAllCSVs.sh
+++ CombineAllCSVs.sh
@@ -4,8 +4,10 @@ python CombineCSVs.py *_Bandits.csv CombinedCSVs/Bandits.csv
 python CombineCSVs.py *_Bandits_Roadside.csv CombinedCSVs/Bandits_Roadside.csv
 python CombineCSVs.py *_Border_Towers.csv CombinedCSVs/Border_Towers.csv
 python CombineCSVs.py *_Guardtowers_Roadside.csv CombinedCSVs/Guardtowers_Roadside.csv
+python CombineCSVs.py *_Mountains.csv CombinedCSVs/Mountains.csv
 python CombineCSVs.py *_Rocks.csv CombinedCSVs/Rocks.csv
 python CombineCSVs.py *_Ruins.csv CombinedCSVs/Ruins.csv
 python CombineCSVs.py *_Ruins_Roadside.csv CombinedCSVs/Ruins_Roadside.csv
 python CombineCSVs.py *_Shrines.csv CombinedCSVs/Shrines.csv
 python CombineCSVs.py *_Shrines_Roadside.csv CombinedCSVs/Shrines_Roadside.csv
+
